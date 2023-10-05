96e4164d0e946be10a37db02454c5d595077e916
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2023-10-05 19:58:58 -0300

    starting-theme-change

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let cálculo = localStorage.getItem('cálculo') || '';
 let cálculoElement = document.querySelector('.js-calculus');
 cálculoElement.innerHTML = localStorage.getItem('cálculo') || '';
+var tema = 'LIGHT'
 
 function calcular(dígito) {
 if (dígito === ' = ') {
@@ -16,5 +17,26 @@ if (dígito === ' = ') {
     cálculo += dígito;
     localStorage.setItem('cálculo', cálculo);
     cálculoElement.innerHTML += dígito;
+    }
 }
-}
+
+const botaoTema = document.querySelector('.js-change-theme');
+const themeIcon = document.querySelector('.js-theme-icon')
+
+function alterarTema() {
+    if (tema === 'LIGHT') {
+        tema = 'DARK'
+
+        botaoTema.classList.add('change-theme-dark')
+        botaoTema.innerHTML = (`<img src="light-theme-icon.png" alt="theme-icon" style="height:20px; margin: 0px 5px;" class="theme-icon js-theme-icon">
+        Change to light mode`)
+
+    } else {
+        tema = 'LIGHT'
+
+        botaoTema.classList.remove('change-theme-dark')
+        botaoTema.innerHTML = (`<img src="light-theme-icon.png" alt="theme-icon" style="height:20px; margin: 0px 5px;" class="theme-icon js-theme-icon">
+        Change to dark mode`)
+
+    }
+};
