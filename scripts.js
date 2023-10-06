7fe521f5be9cc217832cf5136a5468d650b401f9
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2023-10-06 14:23:25 -0300

    removed localStorage for the calculus

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-let cálculo = localStorage.getItem('cálculo') || '';
-let cálculoElement = document.querySelector('.js-calculus');
+// let cálculo = localStorage.getItem('cálculo') || '';
+// let cálculoElement = document.querySelector('.js-calculus');
 cálculoElement.innerHTML = localStorage.getItem('cálculo') || '';
 var tema = localStorage.getItem('tema') || 'LIGHT';
 
@@ -71,17 +71,17 @@ function alterarTema() {
 
 function calcular(dígito) {
     if (dígito === ' = ') {
-        localStorage.setItem('cálculo', cálculo);
+        // localStorage.setItem('cálculo', cálculo);
         cálculoElement.innerHTML += ` = ${eval(cálculo)}`;
     
     } else if (dígito === 'C') {
         cálculo = '';
-        localStorage.removeItem('cálculo');
+        // localStorage.removeItem('cálculo');
         cálculoElement.innerHTML = '';
     
     } else {
         cálculo += dígito;
-        localStorage.setItem('cálculo', cálculo);
+        // localStorage.setItem('cálculo', cálculo);
         cálculoElement.innerHTML += dígito;
         }
     }   
