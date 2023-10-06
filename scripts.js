fe314fa191586c0ce0b64c2c158c9772591f6476
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2023-10-06 13:47:45 -0300

    finished dark/light mode

--- scripts.js
+++ scripts.js
@@ -21,7 +21,19 @@ if (dígito === ' = ') {
 }
 
 const botaoTema = document.querySelector('.js-change-theme');
-const themeIcon = document.querySelector('.js-theme-icon')
+const themeIcon = document.querySelector('.js-theme-icon');
+const bgColor = document.querySelector('.js-body-theme');
+
+const mainContainer = document.querySelector('.js-main-container');
+
+const botaoEqual = document.querySelector('.js-equal-button');
+const botaoC = document.querySelector('.js-C-button');
+const botaoParênteses1 = document.querySelector('.js-P1-button');
+const botaoParênteses2 = document.querySelector('.js-P2-button');
+const botaoDivide = document.querySelector('.js-divide-button');
+const botaoMultiplica = document.querySelector('.js-mult-button')
+const botaoMenos = document.querySelector('.js-menos-button')
+const botaoMais = document.querySelector('.js-mais-button')
 
 function alterarTema() {
     if (tema === 'LIGHT') {
@@ -29,11 +41,37 @@ function alterarTema() {
 
         botaoTema.classList.add('change-theme-dark')
         botaoTema.innerHTML = (`<img src="theme-icon-sun.png" alt="light-theme-icon" style="height:17px; margin: 0px 10px;" class="theme-icon js-theme-icon">
-        Change to light mode (WIP)`) } else {
+        Change to light mode`) 
+        
+        bgColor.classList.add('bodyTheme2');
+        mainContainer.classList.add('main-container2');
+
+        botaoEqual.classList.add('equal2');
+        botaoParênteses1.classList.add('special2');
+        botaoParênteses2.classList.add('special2');
+        botaoDivide.classList.add('special2');
+        botaoMultiplica.classList.add('special2');
+        botaoMenos.classList.add('special2');
+        botaoMais.classList.add('special2');
+        botaoC.classList.add('special2');
+
+    } else {
         tema = 'LIGHT'
 
         botaoTema.classList.remove('change-theme-dark')
         botaoTema.innerHTML = (`<img src="theme-icon-moon.png" alt="dark-theme-icon" style="height:17px; margin: 0px 10px; opacity: 0.85;" class="theme-icon js-theme-icon">
-        Change to dark mode (WIP)`)
+        Change to dark mode`)
+
+        bgColor.classList.remove('bodyTheme2');
+        mainContainer.classList.remove('main-container2');
+
+        botaoEqual.classList.remove('equal2');
+        botaoParênteses1.classList.remove('special2');
+        botaoParênteses2.classList.remove('special2');
+        botaoDivide.classList.remove('special2');
+        botaoMultiplica.classList.remove('special2');
+        botaoMenos.classList.remove('special2');
+        botaoMais.classList.remove('special2');
+        botaoC.classList.remove('special2');
     }
 };
